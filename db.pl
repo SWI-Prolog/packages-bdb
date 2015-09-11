60197451a42f0bb7410966ef44e37107f75bf998
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-09-11 11:11:51 +0200

    DOC: Use PlDoc documentation

--- db.pl
+++ db.pl
@@ -47,15 +47,77 @@
 
 /** <module> Berkeley DB interface
 
-This package realised external storage  of   Prolog  terms  based on the
-Berkeley          DB          library           from          [Sleepycat
-Software](http://www.sleepycat.com/). The DB library  implements modular
-support for the bottom layers of a   database.  The database itself maps
-unconstrained keys onto values. Both key and value are binary blobs.
+This  package  realises  a  binding  to  _Berkeley  DB_,  originally  by
+[Sleepycat   Software](http://www.sleepycat.com/),   now    managed   by
+[Oracle](http://www.oracle.com/technetwork/database/database-technologies/berkeleydb/downloads/index.html).
+The DB library implements modular support  for   the  bottom layers of a
+database. In can be configured  for   single-threaded  access to a file,
+multi-threaded access with  transactions,  remote   access  as  well  as
+database replication.
 
-The SWI-Prolog interface for  DB  allows   for  fast  storage of general
-Prolog terms in the database.
+Berkeley DB is an _embedded_ database. This implies the library provides
+access to a file containing one or more database tables. The Berkeley DB
+database tables are always _binary_, mapping a   _key_ to a _value_. The
+SWI-Prolog interface to Berkeley DB allows for fast storage of arbitrary
+Prolog terms including cycles and constraints in the database.
+
+Accessing a database consists of four steps:
+
+    1. Initialise the DB environment using db_init/1. This step is
+       optional, providing simple non-transactional file access when
+       omitted.
+    2. Open a database using db_open/4, returning a handle to the
+       database.
+    3. Accessing the data using db_put/3, db_get/3, etc.
+    4. Closing a database using db_close/1. When omitted, all open
+       databases are closed on program halt (see at_halt/1).
 */
+
+%%	db_init(+Options) is det.
+%
+%	Initialise the DB package. This must   be  done before the first
+%	call to db_open/4 and at maximum   once.  If db_open/4 is called
+%	without calling db_init/1, default initialisation is used, which
+%	is generally suitable  for  handling   small  databases  without
+%	support for advanced features. Options is a list of options. The
+%	currently supported are listed below.  For details, please refer
+%	to the DB manual.
+%
+%	  - home(+Home)
+%	    Specify the DB home directory, the directory holding the
+%	    database files.
+%	  - config(+ListOfConfig)
+%	    Specify a list of configuration options, each option is of
+%	    the form Name(Value).  Supported config options are:
+%	    - create(+Bool)
+%	      If `true`, create any underlying file as required. By
+%	      default, no new files are created. This option should be
+%	      set for prograns that create new databases.
+%	    - logging(+Bool)
+%	      Enable logging the DB modifications. Logging enables
+%	      recovery of databases in case of system failure. Normally
+%	      it is used in combination with transactions.
+%	    - transactions(+Bool)
+%	      Enable transactions, providing atomicy of changes and
+%	      security. Implies logging and locking. See
+%	      db_transaction/1.
+%	    - server(+Host, [+ServerOptions])
+%	      Initialise the DB package for accessing a remote
+%	      database. Host specifies the name of the machine running
+%	      `berkeley_db_svc`. Optionally additional options may be
+%	      specified:
+%	      - server_timeout(+Seconds)
+%		Specify the timeout time the server uses to determine
+%		that the client has gone. This implies the server will
+%		terminate the connection to this client if this client
+%		does not issue any requests for the indicated time.
+%	      - client_timeout(+Seconds)
+%		Specify the time the client waits for the server to
+%		handle a request.
+%	  - mp_size(+Integer)
+%	    Size of the memory-pool used for caching.
+%	  - mp_mmapsize(+Integer)
+%	    Maximum size of a DB file mapped entirely into memory.
 
 %%	db_open(+File, +Mode, -DB, +Options) is det.
 %
@@ -104,18 +166,19 @@ Prolog terms in the database.
 %	is detected reliably and results in a permission_error
 %	exception.
 
-%%	db_put(+DB, +Key, +Value) is semidet.
+%%	db_put(+DB, +Key, +Value) is det.
 %
-%	Add a new key-value pair to the database. If the database allows
-%	for duplicates this will always succeed,   unless a system error
-%	occurs.
+%	Add a new key-value pair to the   database. If the database does
+%	not allow for duplicates the   possible previous associated with
+%	Key is replaced by Value.
 
-%%	db_del(+DB, ?Key, ?Value).
+%%	db_del(+DB, ?Key, ?Value) is nondet.
 %
 %	Delete the first matching key-value pair   from the database. If
 %	the  database  allows  for   duplicates,    this   predicate  is
-%	non-deterministic. The enumeration performed   by this predicate
-%	is the same as for db_get/3. See also db_delall/3.
+%	non-deterministic, otherwise it is   _semidet_.  The enumeration
+%	performed by this predicate is  the   same  as for db_get/3. See
+%	also db_delall/3.
 
 %%	db_delall(+DB, +Key, ?Value) is det.
 %
@@ -131,11 +194,20 @@ db_delall(DB, Key, Value) :-
 	;   true
 	).
 
-%%	db_get(+DB, ?Key, -Value)
+%%	db_get(+DB, ?Key, -Value) is nondet.
 %
 %	Query the database. If the database   allows for duplicates this
-%	predicate is non-deterministic. Note that if  Key is a term this
-%	matches stored keys that are _variants_ of Key. See =@=/2.
+%	predicate is non-deterministic, otherwise it  is _semidet_. Note
+%	that if Key is  a  term  this   matches  stored  keys  that  are
+%	_variants_ of Key, *not*  unification.   See  =@=/2. Thus, after
+%	db_put(DB, f(X), 42), we get the following query results:
+%
+%	  - db_get(DB, f(Y), V) binds Value to `42`, while `Y` is left
+%	    unbound.
+%	  - db_get(DB, f(a), V) _fails_.
+%	  - db_enum(DB, f(a), V) succeeds, but does not perform any
+%	    indexing, i.e., it enumerates all key-value pairs and
+%	    performs the unification.
 
 %%	db_enum(+DB, -Key, -Value)
 %
@@ -177,6 +249,40 @@ terminate_db :-
 	).
 
 :- at_halt(terminate_db).
+
+%%	db_transaction(:Goal)
+%
+%	Start a transaction, execute Goal and terminate the transaction.
+%	Only if Goal succeeds, the  transaction   is  commited.  If Goal
+%	fails or raises an exception,  the   transaction  is aborted and
+%	db_transaction/1 either fails or  rethrows   the  exception.  Of
+%	special interest is the exception
+%
+%	  ==
+%	  error(package(db, deadlock), _)
+%	  ==
+%
+%	This exception indicates a deadlock was raised  by one of the DB
+%	predicates. Deadlocks may arise if multiple processes or threads
+%	access  the  same  keys   in   a    different   order.   The  DB
+%	infra-structure causes one of  the   processes  involved  in the
+%	deadlock to abort its transaction. This   process  may choose to
+%	restart the transaction.
+%
+%	For example, a DB application  may   define  `{Goal}` to realise
+%	transactions and restart these automatically   is  a deadlock is
+%	raised:
+%
+%	  ==
+%	  {Goal} :-
+%	      catch(db_transaction(Goal), E, true),
+%	      (   var(E)
+%	      ->  true
+%	      ;   E = error(package(db, deadlock), _)
+%	      ->  {Goal}
+%	      ;   throw(E)
+%	      ).
+%	  ==
 
 		 /*******************************
 		 *	       MESSAGES		*
